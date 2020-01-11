cf69de0bb3b8d8e90ff05746bb41c5b60d0ab5b4
JEONG HYUN SEOK <nicewook@hotmail.com>
2020-01-12 06:19:21 +0900

    Feature/float to bytes (#7)
    * code review applied
    
    * Fix unused variable
    
    * Use optLittle

--- conversion.go
+++ conversion.go
@@ -3,7 +3,20 @@ package conversion
 
 import (
 	"fmt"
+
+	"encoding/binary"
+	"errors"
+	"math"
 )
+
+type Endian int
+
+const BigEndian Endian = 0
+const LittleEndian Endian = 1
+
+type Option struct {
+	Endian Endian
+}
 
 // IntToFloat32 converts int to float32
 func IntToFloat32(x int) (float32, error) {
@@ -56,4 +69,64 @@ func unflattenInternal(xs []byte, size int) ([][]byte, error) {
 	}
 
 	return ret, nil
+}
+
+// Float32ToBytes converts float32 to []byte with length 4
+func Float32ToBytes(x float32, o *Option) []byte {
+	b := make([]byte, 4)
+	ux := math.Float32bits(x)
+	if o == nil || o.Endian == BigEndian {
+		binary.BigEndian.PutUint32(b, ux)
+	} else {
+		binary.LittleEndian.PutUint32(b, ux)
+	}
+	return b
+}
+
+// BytesToFloat32 converts []byte to float32
+// length of []byte should be 4 or bigger
+func BytesToFloat32(x []byte, o *Option) (float32, error) {
+	var fx float32
+
+	if len(x) < 4 {
+		return fx, errors.New("length of []byte should be 4 or bigger")
+	}
+
+	var ux uint32
+	if o == nil || o.Endian == BigEndian {
+		ux = binary.BigEndian.Uint32(x)
+	} else {
+		ux = binary.LittleEndian.Uint32(x)
+	}
+	return math.Float32frombits(ux), nil
+}
+
+// Float64ToBytes converts float64 to []byte with length 8
+func Float64ToBytes(x float64, o *Option) []byte {
+	b := make([]byte, 8)
+	ux := math.Float64bits(x)
+	if o == nil || o.Endian == BigEndian {
+		binary.BigEndian.PutUint64(b, ux)
+	} else {
+		binary.LittleEndian.PutUint64(b, ux)
+	}
+	return b
+}
+
+// BytesToFloat64 converts []byte to float64
+// length of []byte should be 8 or bigger
+func BytesToFloat64(x []byte, o *Option) (float64, error) {
+	var fx float64
+
+	if len(x) < 8 {
+		return fx, errors.New("length of []byte should be 4 or bigger")
+	}
+
+	var ux uint64
+	if o == nil || o.Endian == BigEndian {
+		ux = binary.BigEndian.Uint64(x)
+	} else {
+		ux = binary.LittleEndian.Uint64(x)
+	}
+	return math.Float64frombits(ux), nil
 }
